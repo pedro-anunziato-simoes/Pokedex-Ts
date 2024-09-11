55c3f45251825900d762674500b7b7e6812bc9a2
Pedro Henrique <pedro.anunziato.simoes@gmail.com>
2024-09-11 20:09:49 -0300

    primeiro commit

---
+++ src/server.ts
@@ -0,0 +1,42 @@
+import express from 'express';
+import axios from 'axios';
+import path from 'path';
+
+const app = express();
+const port = 3000;
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', async (req, res) => {
+  try {
+    const response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=20');
+    const pokemons = response.data.results;
+    res.render('index', { pokemons });
+  } catch (error) {
+    console.error('Error fetching Pokémon data:', error);
+    res.status(500).send('Erro no servidor');
+  }
+});
+
+app.get('/pokemon/:name', async (req, res) => {
+  const name = req.params.name;
+  try {
+    const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name.toLowerCase()}`);
+    const pokemon = response.data;
+    res.render('pokemon', {
+      pokemon: {
+        id: pokemon.id,
+        name: pokemon.name,
+        stats: pokemon.stats,
+      },
+    });
+  } catch (error) {
+    console.error('Error fetching Pokémon data:', error);
+    res.status(404).send('Pokémon não encontrado');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server rodando na porta http://localhost:${port}`);
+});
